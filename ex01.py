7b0038c92a0b040c345025b6a2a013e8bdbde36b
hmartian <hugo@zmartsoft.com>
2015-08-08 15:07:14 -0500

    Primer ejercicio

---
+++ ex01.py
@@ -0,0 +1,8 @@
+print "Hello world"
+print "Hello Again"
+print "me gusta escribir esto"
+print "Esto es bacano"
+print "Si!!!! Imprimir"
+print "I'd much rather you 'not'"
+print 'I "said" do not touch this'
+
