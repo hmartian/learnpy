39079b4fb477c2673cb092cd617711b756251a78
hmartian <hugo@zmartsoft.com>
2015-08-09 18:32:05 -0500

    otros ejercios

---
+++ ex02.py
@@ -0,0 +1,10 @@
+# Un comentario, sirve para leerlo despues
+# Cualquie cosa que vaya despues del # es ignorada por python
+
+print 'I could have code like this'  #And the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run"
+
+print "This will run"
+
---
+++ ex03_b.py
@@ -0,0 +1,21 @@
+#ex03_b.py
+
+print "I will now count muy chickens:"
+
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+
+print 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + 6
+
+print "Is it true that 3 + 2 < 5 - 7 ?"
+print 3 + 2 < 5 - 7
+
+print "Oh, that's why it's false."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater?", 5 >= -2
+print "Is it less or equal ?", 5 < -2
